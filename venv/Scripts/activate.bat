4e964661a4f7a71a4176b0e2c8b869f6a133e1e8
8moonlight8 <mohamadoubambandiaye@gmail.com>
2026-05-23 03:56:30 +0000

    Lab 3 : API FastAPI avec /health et /predict (v2)

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mbamb\OneDrive\Bureau\L2GLSIC\SEMESTRE2\Integration de modeles IA\sensante\venv
+set VIRTUAL_ENV=C:\Users\mbamb\OneDrive\Bureau\L2GLSIC\SEMESTRE2\Integration de modeles IA\Labs\Lab1\sensante\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
